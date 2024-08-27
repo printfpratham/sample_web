d327013e19191e8032465c3a64ad0dd0e39364bc
pratham.malaviya <pratham.malaviya@siliconsignals.io>
2024-08-27 16:40:54 +0530

    Simple login page

---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Welcome.css';
+
+function Welcome() {
+  return (
+    <div className="welcome-container">
+      <h1>Welcome!</h1>
+      <p>You have successfully logged in.</p>
+    </div>
+  );
+}
+
+export default Welcome;
+
